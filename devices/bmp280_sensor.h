165757756713415777855b31355aa5e79e1062f0
German FreeBSD <german.mundinger@gmail.com>
2024-04-06 14:42:08 +0300

    Fix bmp280

--- devices/bmp280_sensor.h
+++ devices/bmp280_sensor.h
@@ -10,14 +10,16 @@
 
 typedef struct std_error std_error_t;
 
-typedef int (*bmp280_sensor_i2c_callback_t) (uint8_t device_address, uint8_t register_address, uint8_t *array, uint32_t array_size, std_error_t * const error);
+typedef int (*bmp280_sensor_i2c_callback_t) (uint8_t device_address, uint8_t register_address, uint8_t *array, uint32_t array_size, uint32_t timeout_ms, std_error_t * const error);
 typedef void (*bmp280_sensor_delay_callback_t) (uint32_t delay_ms);
 
 typedef struct bmp280_sensor_config
 {
-    bmp280_sensor_i2c_callback_t    read_i2c_callback;
-    bmp280_sensor_i2c_callback_t    write_i2c_callback;
-    bmp280_sensor_delay_callback_t  delay_callback;
+    bmp280_sensor_i2c_callback_t read_i2c_callback;
+    bmp280_sensor_i2c_callback_t write_i2c_callback;
+	uint32_t i2c_timeout_ms;
+
+    bmp280_sensor_delay_callback_t delay_callback;
 
 } bmp280_sensor_config_t;
 
