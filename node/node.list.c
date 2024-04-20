704b7135b647be5dc980f71712fa15988c04308c
German FreeBSD <german.mundinger@gmail.com>
2024-04-20 05:00:01 +0300

    Fix admin node

--- node/node.list.c
+++ node/node.list.c
@@ -8,9 +8,10 @@
 // Nodes
 const uint8_t node_ip_address[NODE_LIST_SIZE][4] =
 {
-    [NODE_B01] = { 192, 168, 100, 110 },
-    [NODE_B02] = { 192, 168, 100, 111 },
-    [NODE_T01] = { 192, 168, 100, 112 }
+	[NODE_ADMIN]	= { 192, 168, 100, 101 },
+    [NODE_B01]		= { 192, 168, 100, 110 },
+    [NODE_B02]		= { 192, 168, 100, 111 },
+    [NODE_T01]		= { 192, 168, 100, 112 }
 };
 
 // Server
@@ -23,11 +24,6 @@ const uint8_t server_ip_address[4] =
 
 // Admin
 const uint16_t admin_port = 2399;
-
-const uint8_t admin_ip_address[4] = 
-{
-    192, 168, 100, 101
-};
 
 const uint8_t netmask[4] =
 {
--- node/node.list.h
+++ node/node.list.h
@@ -11,12 +11,12 @@
 // Nodes
 typedef enum node_id
 {
-    NODE_B01 = 0,
+	NODE_ADMIN = 0,
+    NODE_B01,
     NODE_B02,
 	NODE_T01,
     NODE_LIST_SIZE,
 	NODE_BROADCAST = 255,
-	NODE_ADMIN
 
 } node_id_t;
 
@@ -28,8 +28,8 @@ extern const uint8_t server_ip_address[4];
 
 // Admin
 extern const uint16_t admin_port;
-extern const uint8_t admin_ip_address[4];
 
 extern const uint8_t netmask[4];
 
 #endif // NODE_LIST_H
+
